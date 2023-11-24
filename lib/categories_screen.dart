980e2e668d8c842767f798c13240ef3f8e0db3b4
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-11-24 16:37:32 +0800

    f7 ep 4

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'dummy_data.dart';
+import 'category_item.dart';
+
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
@@ -12,7 +15,9 @@ class CategoriesScreen extends StatelessWidget {
         crossAxisSpacing: 20,
         mainAxisSpacing: 20,
       ),
-      children: const [],
+      children: DUMMY_CATEGORIES
+          .map((catData) => CategoryItem(catData.title, catData.color))
+          .toList(),
     );
   }
 }
