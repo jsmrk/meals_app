d7412c698df52d9b80d525a88f9f574898350c58
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-11-24 16:26:59 +0800

    f7 ep4

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+// ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DeliMeals"),
+      ),
+      body: Center(
+        child: Text("Navigation Time!"),
+      ),
+    );
+  }
+}
