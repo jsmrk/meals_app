d919a8ea6a3ab8b2acec3486f8479bfead0b18bc
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-11-25 18:29:55 +0800

    f7 ep12

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals-screen';
 
@@ -9,6 +11,9 @@ class CategoryMealsScreen extends StatelessWidget {
         ModalRoute.of(context)?.settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
     final categoryId = routeArgs['id'];
+    final categoryMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
       appBar: AppBar(
@@ -16,8 +21,9 @@ class CategoryMealsScreen extends StatelessWidget {
       ),
       body: ListView.builder(
         itemBuilder: (ctx, index) {
-          return;
+          return Text(categoryMeals[index].title);
         },
+        itemCount: categoryMeals.length,
       ),
     );
   }
