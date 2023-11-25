8ef78e24c1bb9b2e051d6211ffc6937eefc622ff
Jess Mark Baguio <119231806+jsmrk@users.noreply.github.com>
2023-11-25 19:15:32 +0800

    f7 ep13

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: prefer_const_constructors, use_key_in_widget_constructors
 
 import 'package:flutter/material.dart';
-import 'package:meals_app/categories_screen.dart';
-import 'package:meals_app/category_meals_screen.dart';
+import 'package:meals_app/screens/categories_screen.dart';
+import 'package:meals_app/screens/category_meals_screen.dart';
 
 void main() {
   runApp(const MyApp());
